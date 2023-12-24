76e97112a5f93f83271f8eaa4268f8c3d8c72de3
Louis-Guillaume MORAND <lgm@lgmorand.com>
2023-12-24 19:02:00 +0100

    first commit

---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Configuration;
+
+namespace CafeReadConf.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; }
+        public string Secret { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            Message = GetProperty("message");
+            Secret = GetProperty("secret");
+        }
+
+
+        private string GetProperty(string key)
+        {
+            string value =  System.Configuration.ConfigurationManager.AppSettings[key];
+            if(string.IsNullOrEmpty(value))
+            {
+                value = Environment.GetEnvironmentVariable(key);
+            }
+            return value;
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
